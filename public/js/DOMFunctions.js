bc3630ff97457c0489575c9e06b1e4c7b9149b9a
Fernando Macias Ruano <fernando@fernando.(none)>
2013-04-29 22:49:51 +0200

    add MIT licence to javaScript files

--- public/js/DOMFunctions.js
+++ public/js/DOMFunctions.js
@@ -1,3 +1,29 @@
+/*
+Copyright 2013 Fernando Macias Ruano <fmaciasruano@gmail.com>
+http://wunderbit.com
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 //The behavior of the DOMNode and these Attributes may differ from one User-Agent to another, so that the Method
 //node.appendChild() does funny things in the user-agents which it belongs to, and also the behavior is different.
 //Because of that, I create the Nodes for my own according a given XMLNode from a text/xml DOMDocument to solve this problem. 
